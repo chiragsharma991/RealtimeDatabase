d73b69e205417133ae4f60195a4334310f53bda3
Chari <chiragsharma991@gmail.com>
2016-12-11 22:47:26 -0800

    testq

--- app/src/main/java/info/androidhive/firebase/User.java
+++ app/src/main/java/info/androidhive/firebase/User.java
@@ -12,7 +12,7 @@ public class User {
 
     public String name;
     public String email;
-    //minesdfsdfs
+    //minesdfsdfssdfs
 
     // Default constructor required for calls to
     // DataSnapshot.getValue(User.class)
